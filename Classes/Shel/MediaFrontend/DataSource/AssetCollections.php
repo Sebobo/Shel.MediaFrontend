249ca2675063e2b6947494f016ec0f6aa12cd2af
Sebastian Helzle <sebastian@helzle.it>
2016-12-27 13:05:30 +0100

    TASK: Apply migration Neos.ContentRepository-20161125012000
    Adjusts code to package renaming from "TYPO3.TYPO3CR" to
    "Neos.ContentRepository"

--- Classes/Shel/MediaFrontend/DataSource/AssetCollections.php
+++ Classes/Shel/MediaFrontend/DataSource/AssetCollections.php
@@ -11,7 +11,7 @@ namespace Shel\MediaFrontend\DataSource;
 use Neos\Media\Domain\Model\AssetCollection;
 use Neos\Media\Domain\Repository\AssetCollectionRepository;
 use Neos\Neos\Service\DataSource\AbstractDataSource;
-use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
 use Neos\Flow\Annotations as Flow;
 
 class AssetCollections extends AbstractDataSource
